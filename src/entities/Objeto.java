55d0f63e3c9541457156d069a45182a9985e6081
ksjenger <kauan.jenger@gmail.com>
2019-06-20 02:16:55 -0300

    Criacao da Classe Objeto

---
+++ src/entities/Objeto.java
@@ -0,0 +1,302 @@
+package entities;
+
+public class Objeto {
+
+    private String destinatario;
+    private String endereco;
+    private String numero;
+    private String complemento;
+    private String bairro;
+    private String cidade;
+    private String uf;
+    private String cep;
+    private String telefone;
+    private String financeiro;
+    private String registro;
+    private Integer peso;
+    private Integer altura;
+    private Integer largura;
+    private Integer comprimento;
+    private Integer cubico;
+    private String nota;
+    private String serie_nota;
+    private Integer declarado;
+    private Integer valor_cobrar;
+    private String adicionais;
+    private Integer contrato;
+    private Integer administrativo;
+    private Integer cartao;
+    private String email;
+    private String observacao_1;
+    private String observacao_2;
+    private String observacao_3;
+    private Integer telefone_celular;
+
+    public Objeto() {
+    }
+
+    public Objeto(String destinatario, String endereco, String numero, String complemento, String bairro, String cidade, String uf, String cep, String telefone, String financeiro, String registro, Integer peso, Integer altura, Integer largura, Integer comprimento, Integer cubico, String nota, String serie_nota, Integer declarado, Integer valor_cobrar, String adicionais, Integer contrato, Integer administrativo, Integer cartao, String email, String observacao_1, String observacao_2, String observacao_3, Integer telefone_celular) {
+        this.destinatario = destinatario;
+        this.endereco = endereco;
+        this.numero = numero;
+        this.complemento = complemento;
+        this.bairro = bairro;
+        this.cidade = cidade;
+        this.uf = uf;
+        this.cep = cep;
+        this.telefone = telefone;
+        this.financeiro = financeiro;
+        this.registro = registro;
+        this.peso = peso;
+        this.altura = altura;
+        this.largura = largura;
+        this.comprimento = comprimento;
+        this.cubico = cubico;
+        this.nota = nota;
+        this.serie_nota = serie_nota;
+        this.declarado = declarado;
+        this.valor_cobrar = valor_cobrar;
+        this.adicionais = adicionais;
+        this.contrato = contrato;
+        this.administrativo = administrativo;
+        this.cartao = cartao;
+        this.email = email;
+        this.observacao_1 = observacao_1;
+        this.observacao_2 = observacao_2;
+        this.observacao_3 = observacao_3;
+        this.telefone_celular = telefone_celular;
+    }
+
+    public String getDestinatario() {
+        return destinatario;
+    }
+
+    public void setDestinatario(String destinatario) {
+        this.destinatario = destinatario;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public String getNumero() {
+        return numero;
+    }
+
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+
+    public String getComplemento() {
+        return complemento;
+    }
+
+    public void setComplemento(String complemento) {
+        this.complemento = complemento;
+    }
+
+    public String getBairro() {
+        return bairro;
+    }
+
+    public void setBairro(String bairro) {
+        this.bairro = bairro;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    public String getUf() {
+        return uf;
+    }
+
+    public void setUf(String uf) {
+        this.uf = uf;
+    }
+
+    public String getCep() {
+        return cep;
+    }
+
+    public void setCep(String cep) {
+        this.cep = cep;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getFinanceiro() {
+        return financeiro;
+    }
+
+    public void setFinanceiro(String financeiro) {
+        this.financeiro = financeiro;
+    }
+
+    public String getRegistro() {
+        return registro;
+    }
+
+    public void setRegistro(String registro) {
+        this.registro = registro;
+    }
+
+    public Integer getPeso() {
+        return peso;
+    }
+
+    public void setPeso(Integer peso) {
+        this.peso = peso;
+    }
+
+    public Integer getAltura() {
+        return altura;
+    }
+
+    public void setAltura(Integer altura) {
+        this.altura = altura;
+    }
+
+    public Integer getLargura() {
+        return largura;
+    }
+
+    public void setLargura(Integer largura) {
+        this.largura = largura;
+    }
+
+    public Integer getComprimento() {
+        return comprimento;
+    }
+
+    public void setComprimento(Integer comprimento) {
+        this.comprimento = comprimento;
+    }
+
+    public Integer getCubico() {
+        return cubico;
+    }
+
+    public void setCubico(Integer cubico) {
+        this.cubico = cubico;
+    }
+
+    public String getNota() {
+        return nota;
+    }
+
+    public void setNota(String nota) {
+        this.nota = nota;
+    }
+
+    public String getSerie_nota() {
+        return serie_nota;
+    }
+
+    public void setSerie_nota(String serie_nota) {
+        this.serie_nota = serie_nota;
+    }
+
+    public Integer getDeclarado() {
+        return declarado;
+    }
+
+    public void setDeclarado(Integer declarado) {
+        this.declarado = declarado;
+    }
+
+    public Integer getValor_cobrar() {
+        return valor_cobrar;
+    }
+
+    public void setValor_cobrar(Integer valor_cobrar) {
+        this.valor_cobrar = valor_cobrar;
+    }
+
+    public String getAdicionais() {
+        return adicionais;
+    }
+
+    public void setAdicionais(String adicionais) {
+        this.adicionais = adicionais;
+    }
+
+    public Integer getContrato() {
+        return contrato;
+    }
+
+    public void setContrato(Integer contrato) {
+        this.contrato = contrato;
+    }
+
+    public Integer getAdministrativo() {
+        return administrativo;
+    }
+
+    public void setAdministrativo(Integer administrativo) {
+        this.administrativo = administrativo;
+    }
+
+    public Integer getCartao() {
+        return cartao;
+    }
+
+    public void setCartao(Integer cartao) {
+        this.cartao = cartao;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getObservacao_1() {
+        return observacao_1;
+    }
+
+    public void setObservacao_1(String observacao_1) {
+        this.observacao_1 = observacao_1;
+    }
+
+    public String getObservacao_2() {
+        return observacao_2;
+    }
+
+    public void setObservacao_2(String observacao_2) {
+        this.observacao_2 = observacao_2;
+    }
+
+    public String getObservacao_3() {
+        return observacao_3;
+    }
+
+    public void setObservacao_3(String observacao_3) {
+        this.observacao_3 = observacao_3;
+    }
+
+    public Integer getTelefone_celular() {
+        return telefone_celular;
+    }
+
+    public void setTelefone_celular(Integer telefone_celular) {
+        this.telefone_celular = telefone_celular;
+    }
+    
+}
